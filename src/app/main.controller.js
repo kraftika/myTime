ba27d4c1bcb04bb3d51f309ca74a43dad22dd064
Ionut Pistol <ionut.pistol@gmail.com>
2017-03-29 01:38:15 +0300

    Clean up the commented code

--- src/app/main.controller.js
+++ src/app/main.controller.js
@@ -1,47 +1,8 @@
-// import { ModalComponent } from './modal/modal.component';
-
 export default class MainController {
     /*ngInject*/
     constructor($uibModal) {
-        // this.company = { 
-        //     id: 40426 
-        // };
-
-        // this.location = null;
-        // this.companyLocation = {};
-        // this.locationSelected = false;
-
         this.$uibModal = $uibModal;
-        // this.items = ['item1', 'item2'];
     }
-
-    // onSelectedLocation(event) {
-    //     this.location = event.location;
-    //     this.companyLocation = {
-    //         company: this.company.id,
-    //         location: event.location.id
-    //     };
-
-    //     this.locationSelected = true;
-    // }
-
-    // onSelectedService(event) {
-    //     this.service = event.service;
-    //     console.log('Selected service: ', this.service);
-    // }
-
-    // goBack() {
-    //     this.locationSelected = false;
-    //     this.companyLocation.location = null;
-    // }
-
-    // ok() {
-    //     console.log('OK');
-    // }
-
-    // cancel() {
-    //     console.log('cancel');
-    // }
 
     openComponentModal() {
         var modalInstance = this.$uibModal.open({
